997ac3c0510871ce11486146b7d3f599599c36ee
Esteban Gonzalez <estebangonzalez@MacBook-Pro-de-Esteban.local>
2020-06-09 22:16:53 -0400

    actualizacion emprendedor1.rb

--- rentabilidad/emprendedor1.rb
+++ rentabilidad/emprendedor1.rb
@@ -2,6 +2,10 @@ precio_venta = ARGV[0].to_i
 num_usuarios = ARGV[1].to_i
 gastos  = ARGV[2].to_i
 
-salida = (precio_venta*num_usuarios)-gastos
-
-puts "Sus utilidades son #{salida}"
+utilidades = (precio_venta*num_usuarios)-gastos
+if utilidades > 0
+    salida =utilidades - ((35 * utilidades)/100)
+    puts "Sus utilidades son #{salida}"
+else
+    puts "Sus utilizades #{utilidades}"
+end
