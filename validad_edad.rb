170347899081f44291c8326379a23370e81de9ee
Esteban Gonzalez <estebangonzalez@MacBook-Pro-de-Esteban.local>
2020-06-11 23:49:48 -0400

    correcion validad_edad.rb

--- validad_edad.rb
+++ validad_edad.rb
@@ -1,8 +1,8 @@
 def validar_edad(edad_input)
     if edad_input >=18
-        print "#{edad_input} es mayor o igual 18\n"
+        puts "#{edad_input} es mayor o igual 18\n"
     else
-        print "#{edad_input} es menor a 18\n"
+        puts "#{edad_input} es menor a 18\n"
     end
 end
 
